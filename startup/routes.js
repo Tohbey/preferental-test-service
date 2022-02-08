7fb9d62e185e8655757fbef336b3e33dea8bd28a
Fafowora Oluwatobiloba Kayode <fafoworatobi25@gmail.com>
2022-02-08 09:48:38 +0100

    update in routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const auth = require('../routes/auth');
 const user = require('../routes/user');
+const book = require('../routes/book');
+const store = require('../routes/store');
 const { JsonResponse } = require("../lib/apiResponse");
 
 module.exports = function (app) {
@@ -8,6 +10,8 @@ module.exports = function (app) {
     app.use(express.urlencoded({limit: '50mb'}));
     app.use("/api/v1/auth", auth);
     app.use("/api/v1/user", user);
+    app.use("/api/v1/book", book);
+    app.use("/api/v1/store", store);
 
     app.use((req, res, next) => {
         return JsonResponse(res, 404, "API endpoint not found")
